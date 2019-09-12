6284f7218574232662b81be5b7dc4c57073fb263
zzzz465 <zzzz465@naver.com>
2019-09-12 21:53:09 +0900

    backup

--- NewDefExtractor.GUI/View.xaml.cs
+++ NewDefExtractor.GUI/View.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace NewDefExtractor.GUI
 {
@@ -26,5 +27,25 @@ namespace NewDefExtractor.GUI
             InitializeComponent();
 			this.DataContext = this;
         }
+
+		public void LoadMods()
+		{
+			string WorkshopFolderPath = ProgramConfig.instance.WorkshopFolderPath;
+			string LocalFolderPath = ProgramConfig.instance.LocalFolderPath;
+
+			List<DirectoryInfo> ModFolders = new List<DirectoryInfo>();
+			Directory.GetDirectories(WorkshopFolderPath).ToList().ForEach(item =>
+			{
+				DirectoryInfo info = new DirectoryInfo(item);
+				ModFolders.Add(info);
+			});
+
+			Directory.GetDirectories(LocalFolderPath).ToList().ForEach(item =>
+			{
+				DirectoryInfo info = new DirectoryInfo(item);
+				ModFolders.Add(info);
+			});
+
+		}
     }
 }
